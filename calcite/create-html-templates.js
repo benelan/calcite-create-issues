a51e2c4066295f619f4fca0448a450df1a92c219
Ben Elan <benelan@users.noreply.github.com>
2022-05-05 12:07:52 -0700

    feat: add script to generate html for each component

---
+++ calcite/create-html-templates.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+import { resolve } from "path";
+import { URL } from "url";
+import { mkdir, readdir, writeFile } from "fs/promises";
+
+const skip = [
+  "functional",
+  "color-picker-hex-input",
+  "color-picker-swatch",
+  "date-picker-day",
+  "date-picker-month",
+  "date-picker-month-header",
+  "graph",
+  "handle",
+  "sortable-list",
+];
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
+const componentsPath = resolve(
+  __dirname,
+  "calcite-components",
+  "src",
+  "components"
+);
+
+(async () => {
+  try {
+    const outdir = resolve(__dirname, "html-templates");
+    await mkdir(outdir, { recursive: true });
+
+    const components = (await readdir(componentsPath, { withFileTypes: true }))
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name)
+      .filter((c) => !skip.includes(c));
+
+    for (const component of components) {
+      const outfile = resolve(outdir, `${component}.html`);
+      await writeFile(outfile, getHTML(component));
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
+
+function getHTML(component) {
+  return `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <title>calcite-${component} template</title>
+    <script
+      type="module"
+      src="https://unpkg.com/@esri/calcite-components@1.0.0-beta.81/dist/calcite/calcite.esm.js"
+    ></script>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://unpkg.com/@esri/calcite-components@1.0.0-beta.81/dist/calcite/calcite.css"
+    />
+  </head>
+  <style>
+  </style>
+  <body>
+    <calcite-${component}></calcite-${component}>
+  </body>
+  <script>
+  </script>
+</html>`;
+}
